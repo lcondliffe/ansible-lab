ea9d497706d1bd5ae1253b7385c9ce89a2729580
Luke Williams <l.williams@derby.ac.uk>
2020-05-31 20:40:39 +0100

    Paramatise the AWS region to create the lab in

--- vars.tf
+++ vars.tf
@@ -1,3 +1,6 @@
 variable "ssh_key" {
   description = "The .pem key downloaded from AWS, to be used for SSH access to lab nodes."
 }
+variable "aws_region" {
+  description = "The AWS region to create the lab in."
+}
