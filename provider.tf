3206153c87c3c6d5afcf5e3059ef370a8f4da0e9
Luke Williams <l.williams@derby.ac.uk>
2020-06-04 20:17:04 +0100

    Provider comments

--- provider.tf
+++ provider.tf
@@ -3,7 +3,6 @@ provider "aws" {
   version = "2.7"
   region  = var.aws_region
 
-  # Export AWS Credentials as environment vars:
-  # export AWS_ACCESS_KEY_ID="anaccesskey"
-  # export AWS_SECRET_ACCESS_KEY="asecretkey"
+  # An authentication method is required to your AWS subscription. For details:
+  # https://www.terraform.io/docs/providers/aws/index.html
 }
